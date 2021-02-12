8d6c5290fce80568485b708534d4c49066926764
Mohamed Yassine YAHYAOUI <yahyaouiy1997@gmail.com>
2021-02-12 12:39:22 +0100

    first commit

---
+++ src/fr/upem/net/tcp/ClientEOS.java
@@ -0,0 +1,126 @@
+package fr.upem.net.tcp;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.util.LinkedList;
+import java.util.Objects;
+import java.util.logging.Logger;
+
+public class ClientEOS {
+
+	public static final Charset UTF8_CHARSET = Charset.forName("UTF-8");
+	public static final int BUFFER_SIZE = 1024;
+	public static final Logger logger = Logger.getLogger(ClientEOS.class.getName());
+
+	/**
+	 * This method: - connect to server - writes the bytes corresponding to request
+	 * in UTF8 - closes the write-channel to the server - stores the bufferSize
+	 * first bytes of server response - return the corresponding string in UTF8
+	 *
+	 * @param request
+	 * @param server
+	 * @param bufferSize
+	 * @return the UTF8 string corresponding to bufferSize first bytes of server
+	 *         response
+	 * @throws IOException
+	 */
+
+	public static String getFixedSizeResponse(String request, SocketAddress server, int bufferSize) throws IOException {
+		SocketChannel sc = SocketChannel.open();
+		sc.connect(server);
+		ByteBuffer snedBuffer = ByteBuffer.allocate(BUFFER_SIZE);
+		snedBuffer.put(UTF8_CHARSET.encode(request));
+		snedBuffer.flip();
+		sc.write(snedBuffer);
+		sc.shutdownOutput();
+
+		ByteBuffer recieveBuff = ByteBuffer.allocate(bufferSize);
+
+		while (sc.read(recieveBuff) != -1) {
+			if (!recieveBuff.hasRemaining()) {
+				break;
+			}
+		}
+		sc.close();
+		recieveBuff.flip();
+		return UTF8_CHARSET.decode(recieveBuff).toString();
+
+	}
+
+	/**
+	 * This method: - connect to server - writes the bytes corresponding to request
+	 * in UTF8 - closes the write-channel to the server - reads and stores all bytes
+	 * from server until read-channel is closed - return the corresponding string in
+	 * UTF8
+	 *
+	 * @param request
+	 * @param server
+	 * @return the UTF8 string corresponding the full response of the server
+	 * @throws IOException
+	 */
+
+	public static String getUnboundedResponse(String request, SocketAddress server) throws IOException {
+
+		SocketChannel sc = SocketChannel.open();
+		sc.connect(server);
+		ByteBuffer snedBuffer = ByteBuffer.allocate(BUFFER_SIZE);
+		snedBuffer.put(UTF8_CHARSET.encode(request));
+		snedBuffer.flip();
+		sc.write(snedBuffer);
+		sc.shutdownOutput();
+		var currentBuffSize = BUFFER_SIZE;
+		ByteBuffer recieveBuff = ByteBuffer.allocate(currentBuffSize);
+
+		while (sc.read(recieveBuff) != -1) {
+			if (!recieveBuff.hasRemaining()) {
+				currentBuffSize *= 2;
+				ByteBuffer tmp = ByteBuffer.allocate(currentBuffSize);
+				recieveBuff.flip();
+				tmp.put(recieveBuff);
+				recieveBuff = tmp;
+			}
+		}
+		sc.close();
+		recieveBuff.flip();
+		return UTF8_CHARSET.decode(recieveBuff).toString();
+	}
+
+	/**
+	 * Fill the workspace of the Bytebuffer with bytes read from sc.
+	 *
+	 * @param sc
+	 * @param bb
+	 * @return false if read returned -1 at some point and true otherwise
+	 * @throws IOException
+	 */
+	static boolean readFully(SocketChannel sc, ByteBuffer bb) throws IOException {
+		Objects.requireNonNull(bb);
+		Objects.requireNonNull(sc);
+		while(true) {
+			if(sc.read(bb) == -1) {
+				return false;
+			};
+			if (!bb.hasRemaining()) {
+				
+				currentBuffSize *= 2;
+				ByteBuffer tmp = ByteBuffer.allocate(currentBuffSize);
+				recieveBuff.flip();
+				tmp.put(recieveBuff);
+				recieveBuff = tmp;
+			}
+			
+		}
+		return true;
+	}
+
+	public static void main(String[] args) throws IOException {
+		InetSocketAddress google = new InetSocketAddress("www.google.fr", 80);
+		//System.out.println(getFixedSizeResponse("GET / HTTP/1.1\r\nHost: www.google.fr\r\n\r\n", google, 512));
+		 System.out.println(getUnboundedResponse("GET / HTTP/1.1\r\nHost: www.google.fr\r\n\r\n",
+		 google));
+	}
+}
---
+++ src/fr/upem/net/tcp/ClientLongSum.java
@@ -0,0 +1,95 @@
+package fr.upem.net.tcp;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.*;
+import java.util.logging.Logger;
+
+public class ClientLongSum {
+
+	private static final int BUFFER_SIZE = 1024;
+	public static final Logger logger = Logger.getLogger(ClientLongSum.class.getName());
+	
+	
+	
+	private static ArrayList<Long> randomLongList(int size) {
+		Random rng = new Random();
+		ArrayList<Long> list = new ArrayList<>(size);
+		for (int i = 0; i < size; i++) {
+			list.add(rng.nextLong());
+		}
+		return list;
+	}
+
+	
+	static boolean readFully(SocketChannel sc, ByteBuffer bb) throws IOException {
+        while(bb.hasRemaining()) {
+            if (sc.read(bb)==-1){
+                logger.info("Input stream closed");
+                return false;
+            }
+        }
+        return true;
+    }
+	
+	private static boolean checkSum(List<Long> list, long response) {
+		long sum = 0;
+		for (long l : list)
+			sum += l;
+		return sum == response;
+	}
+
+	/**
+	 * Write all the longs in list in BigEndian on the server and read the long sent
+	 * by the server and returns it
+	 *
+	 * returns Optional.empty if the protocol is not followed by the server but no
+	 * IOException is thrown
+	 *
+	 * @param sc
+	 * @param list
+	 * @return
+	 * @throws IOException
+	 */
+	private static Optional<Long> requestSumForList(SocketChannel sc, List<Long> list) throws IOException {
+
+		ByteBuffer bb = ByteBuffer.allocate(BUFFER_SIZE);
+		bb.putInt(list.size());
+		for(var l : list) {
+			bb.putLong(l);
+		}
+		bb.flip();
+		sc.write(bb);
+		//sc.shutdownOutput();
+		
+		ByteBuffer recieveBb = ByteBuffer.allocate(Long.BYTES);
+		
+		if(readFully(sc, recieveBb)) {
+			recieveBb.flip();
+			return Optional.of(recieveBb.getLong());
+		}
+		
+		return Optional.empty();
+	}
+
+	public static void main(String[] args) throws IOException {
+		InetSocketAddress server = new InetSocketAddress(args[0], Integer.valueOf(args[1]));
+		try (SocketChannel sc = SocketChannel.open(server)) {
+			for (int i = 0; i < 5; i++) {
+				ArrayList<Long> list = randomLongList(50);
+
+				Optional<Long> l = requestSumForList(sc, list);
+				if (!l.isPresent()) {
+					System.err.println("Connection with server lost.");
+					return;
+				}
+				if (!checkSum(list, l.get())) {
+					System.err.println("Oups! Something wrong happens!");
+				}
+			}
+			System.err.println("Everything seems ok");
+		}
+	}
+}
